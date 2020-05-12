d0f311809fa2c5694d7518a57267026e5bf1f09a
Melody Soriano <mel.soriano6123@gmail.com>
2020-05-12 14:18:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,11 +3,11 @@ def game_hash
   hash = {
     home: {team_name: "Brooklyn Nets",
           colors: ["Black","White"],
-          players: [{player_name: , number: , shoe: , points: , rebounds:},
-                    {player_name: , number: , shoe: , points: , rebounds:},
-                    {player_name: , number: , shoe: , points: , rebounds:},
-                    {player_name: , number: , shoe: , points: , rebounds:},
-                    {player_name: , number: , shoe: , points: , rebounds:}],
+          players: [{player_name: , number: , shoe: , points: , rebounds: , assists: , steals: , blocks: , slam_dunks:},
+                    {player_name: , number: , shoe: , points: , rebounds: , assists: , steals: , blocks: , slam_dunks:},
+                    {player_name: , number: , shoe: , points: , rebounds: , assists: , steals: , blocks: , slam_dunks:},
+                    {player_name: , number: , shoe: , points: , rebounds: , assists: , steals: , blocks: , slam_dunks:},
+                    {player_name: , number: , shoe: , points: , rebounds: , assists: , steals: , blocks: , slam_dunks:}],
     away: {team_name: "",
           colors: ["",""],
           players: [{},{}]}
