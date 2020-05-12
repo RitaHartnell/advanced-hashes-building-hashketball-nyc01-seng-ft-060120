1da06a608f0638fe2e792e7398c952e71d32dc92
Melody Soriano <mel.soriano6123@gmail.com>
2020-05-12 14:06:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 # Write your code here!
 def game_hash
   hash = {
-    home: {team_name: "",
+    home: {team_name: "Brooklyn Nets",
           colors: ["",""],
           players: [{},{}]},
     away: {team_name: "",
